ce3d4dfa37406c0f795d98b1d5c016e4c21d236e
Samuel <samuel.dupont@arteac-lab.fr>
2024-03-05 10:49:42 +0100

    :sparkles: import and through a workaround relative import. This update makes it easier to understand different approaches of module imports in Python. Inspection and print statements have been added to show the function's location in both scenarios.

--- examples/exemple.py
+++ examples/exemple.py
@@ -1,7 +1,30 @@
-from mypackage.module0 import calculate_rms
-
+import sys
+from pathlib import Path
+import inspect
 
 if __name__ == "__main__":
     numbers = [2, 4, 6, 8, 10]
+
+    #%% unproper relative way
+    # This script demonstrates a relative import, where a package that is not in the root or a subfolder of the Python
+    # program is forcibly loaded.
+    # Typically, individuals place a file containing functions in the root or a subfolder of the Python script being
+    # used and import it without utilizing this workaround.
+    relative_path = str(Path(__file__).resolve().parent.parent / 'src')
+    sys.path.append(relative_path)
+    from src.mypackage.module0 import calculate_rms
+    sys.path.remove(relative_path)
+
+    # print where is located the calculate_rms function
     rms_value = calculate_rms(numbers)
-    print(f"RMS of {numbers} is {rms_value}")
+    print(f"Package relative local import : RMS of {numbers} is {rms_value}")
+    print(f"calculate_rms is located in: {inspect.getfile(calculate_rms)}")
+
+    #%% direct import and easy way :
+    from mypackage.module0 import calculate_rms
+
+    # print where is located the calculate_rms function
+    rms_value = calculate_rms(numbers)
+    print(f"/nPackage direct import : RMS of {numbers} is {rms_value}")
+    print(f"calculate_rms is located in: {inspect.getfile(calculate_rms)}")
+    print(f'The function is actually installed into your python environnement now')
